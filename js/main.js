7951cb6cd82879454ec0026c9ea68e7fc28d2997
Austin Erlandson <austin@erlandson.com>
2015-03-04 16:01:48 -0600

    up-to-date

--- js/main.js
+++ js/main.js
@@ -3,41 +3,60 @@ angular
   .controller('TasController', function () {
     var vm = this;
 
+    vm.cohortOptions = [
+      'N/A',
+      'One',
+      'Two',
+      'Three',
+      'Four',
+      'Five',
+      'Six',
+      'Seven',
+      'Eight',
+      'Nine',
+      'Ten'
+    ];
+
     vm.data = [
       {
         nickName: 'TAdam',
         name: 'Adam',
         firstName: 'Adam',
         lastName: 'Kèésecker',
-        current: true
+       current: true,
+        cohort: 5
       },
       {
         nickName: 'ZAdam',
         name: 'Adam',
         firstName: 'Zöe',
         lastName: 'Ames',
-        current: true
+        current: true,
+        cohort: 6
       },
       {
         nickName: 'JuAdam',
         name: 'Adam',
         firstName: 'Juan',
         lastName: 'Rødrįguež',
-        current: true
+        current: true,
+        cohort: 6
       },
       {
         nickName: 'BrAdam',
         name: 'Adam',
         firstName: 'Brian',
         lastName: 'Hiått',
-        current: false
+        current: false,
+        cohort: 6
       },
       {
         nickName: 'BAdam',
         name: 'Adam',
         firstName: 'Adam',
         lastName: 'Barñhærd',
-        current: false
+        current: false,
+        cohort: 6
       }
     ];
 
@@ -46,7 +65,6 @@ angular
     vm.addTA = function () {
       vm.newTA.name = 'Adam';
       vm.newTA.nickName = vm.newTA.firstName[0].toUpperCase() + 'Adam';
-
       vm.data.push(vm.newTA);
       vm.newTA = {};
     };
